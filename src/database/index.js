e4e70e8ab3b3a6ad99121937b615a236d66e9c12
kamilladalmann <kamilladalmann@hotmail.com>
2018-11-29 19:31:39 -0200

    banco

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
-//mongoose.connect('mongodb://kamilladallmann:admin19*@ds051740.mlab.com:51740/nodeback', {useNewUrlParser: true});
-mongoose.connect('mongodb://localhost/nodeback', {useNewUrlParser: true});
+mongoose.connect('mongodb://kamilladallmann:admin19*@ds051740.mlab.com:51740/nodeback', {useNewUrlParser: true});
+//mongoose.connect('mongodb://localhost/nodeback', {useNewUrlParser: true});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
