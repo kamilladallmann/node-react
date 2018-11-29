bd711e8db1c271097d9c4ffb7dbe3dd90f8bd39f
kamilladallmann <kamilladallmann@hotmail.com>
2018-11-29 11:02:02 -0200

    cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -6,6 +7,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use('/uploads', express.static('uploads'));
+app.use(cors());
 require('./controllers/index')(app);
 
 var porta = process.env.PORT || 8080;
