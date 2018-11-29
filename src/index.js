a372fb83162a2a5464d0d3ca644b9afbb3695569
kamilladalmann <kamilladalmann@hotmail.com>
2018-11-28 23:29:05 -0200

    nem sei

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const porta = process.env.PORT || 8080;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use('/uploads', express.static('uploads'));
-require('./controllers/index')(app);
+require('./controllers/authController')(app);
+require('./controllers/photoController')(app);
 
-var porta = process.env.PORT || 8080;
+
 app.listen(porta);
 
 //app.listen(3000);
