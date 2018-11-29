5190af007086476eb8b56e5ef250d8e1abb643ad
kamilladalmann <kamilladalmann@hotmail.com>
2018-11-28 22:12:25 -0200

    back

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use('/uploads', express.static('uploads'));
+require('./app/controllers/index')(app);
+
+//var porta = process.env.PORT || 8080;
+//app.listen(porta);
+
+app.listen(3000);
